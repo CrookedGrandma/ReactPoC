4cb00b57f93f5c683ebef49389e2bff9f6f82efb
KasperNooteboom <kasper.nooteboom@partnersinenergie.nl>
2025-03-12 13:51:22 +0100

    Add bundle analyzer

--- webpack/webpack.config.ts
+++ webpack/webpack.config.ts
@@ -1,10 +1,14 @@
 import baseConfig from "./webpack.base.ts";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import { merge } from "webpack-merge";
 import webpack from "webpack";
 
 const conf = async (env: AnyObject) => {
     const envConfig = (await import(`./webpack.${env.env}.ts`)).default as webpack.Configuration;
-    return merge([baseConfig, envConfig]);
+    const analysisConfig: webpack.Configuration = env.analysis == "true"
+        ? { plugins: [new BundleAnalyzerPlugin()] }
+        : {};
+    return merge([baseConfig, envConfig, analysisConfig]);
 };
 
 export default conf;
