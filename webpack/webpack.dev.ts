8deeb79b9b48f090eac9c42f783175ba8ebba17b
KasperNooteboom <kasper.nooteboom@partnersinenergie.nl>
2025-03-12 10:41:03 +0100

    Add first stateful object & react-refresh

--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -1,6 +1,17 @@
 import conf from "./webpack.base.ts";
+import webpackDevServer from "webpack-dev-server";
+import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 conf.mode = "development";
 conf.devtool = "inline-source-map";
+
+conf.devServer = {
+    hot: true,
+    open: true,
+} as webpackDevServer.Configuration;
+
+if (!conf.plugins)
+    conf.plugins = [];
+conf.plugins.push(new ReactRefreshPlugin())
 
 export default conf;
